52c740ffaa163ab7fa17b6de4d46aba804aa5144
Ian Moffett <ian@mirocom.org>
2026-03-14 10:06:08 -0400

    Srm/Foreign: Sdk: Add POSIX SHIM / compat headers
    This commit introduces some minimal POSIX shim headers that are to be
    used by certain external subsystems that require it. Internal Mizion
    subsystems should NOT use these headers at all and should instead rely
    on Sdk/*.h
    
    Signed-off-by: Ian Moffett <ian@mirocom.org>

---
+++ System/Srm/Foreign/Sdk/Include/Sdk/Compat/stdint.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2026, Mirocom Laboratories
+ * Provided under the BSD-3 clause
+ */
+
+/*
+ * Description:
+ *      POSIX stdint shim header
+ *  Author:
+ *      Ian M. Moffett <ian@mirocom.org>
+ */
+
+#ifndef _COMPAT_STDINT_H_
+#define _COMPAT_STDINT_H_ 1
+
+
+/* Unsigned types */
+typedef unsigned char       __uint8_t;
+typedef unsigned short      __uint16_t;
+typedef unsigned int        __uint32_t;
+typedef unsigned long long  __uint64_t;
+typedef __uint64_t          __size_t;
+typedef __uint64_t          __uintptr_t;
+
+/* Signed types */
+typedef int         __int8_t;
+typedef short       __int16_t;
+typedef int         __int32_t;
+typedef long long   __int64_t;
+typedef __int64_t   __ssize_t;
+typedef __ssize_t   __off_t;
+
+#if defined(_KERNEL)
+#ifndef uint8_t
+typedef __uint8_t   uint8_t;
+#endif  /* !uint8_t */
+
+#ifndef uint16_t
+typedef __uint16_t  uint16_t;
+#endif  /* !uint16_t */
+
+#ifndef uint32_t
+typedef __uint32_t  uint32_t;
+#endif  /* !uint32_t */
+
+#ifndef uint64_t
+typedef __uint64_t  uint64_t;
+#endif  /* !uint64_t */
+
+#ifndef uintptr_t
+typedef __uint64_t  uintptr_t;
+#endif  /* !uintptr_t */
+
+#ifndef size_t
+typedef __size_t  size_t;
+#endif  /* !size_t */
+
+#ifndef off_t
+typedef __size_t  off_t;
+#endif  /* !off_t */
+
+#ifndef int8_t
+typedef __int8_t    int8_t;
+#endif  /* !int8_t */
+
+#ifndef int16_t
+typedef __int16_t   int16_t;
+#endif  /* !int16_t */
+
+#ifndef int16_t
+typedef __int32_t   int32_t;
+#endif  /* !int32_t */
+
+#ifndef int64_t
+typedef __int64_t   int64_t;
+#endif  /* !int64_t */
+
+#ifndef ssize_t
+typedef __ssize_t ssize_t;
+#endif  /* !ssize_t */
+
+#ifndef intmax_t
+typedef __ssize_t intmax_t;
+#endif  /* !intmax_t */
+#endif  /* _KERNEL */
+
+#endif  /* !_COMPAT_STDINT_H_ */

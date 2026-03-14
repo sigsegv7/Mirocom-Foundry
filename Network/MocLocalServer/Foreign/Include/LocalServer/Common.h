ed50d67a149c6713bb3fd29c0fffa50052e43f94
Ian Moffett <ian@mirocom.org>
2026-03-13 20:20:57 -0400

    MocLocalServer: Foreign: Add common defs
    Signed-off-by: Ian Moffett <ian@mirocom.org>

---
+++ Network/MocLocalServer/Foreign/Include/LocalServer/Common.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2026, Mirocom Laboratories
+ * Provided under the BSD-3 clause
+ */
+
+/*
+ * Description:
+ *      Mirocom Local Server common definitions
+ * Author:
+ *      Ian M. Moffett <ian@mirocomMirocom Laboratories.org>
+ */
+
+#ifndef LOCALSERVER_COMMON_H
+#define LOCALSERVER_COMMON_H 1
+
+/*
+ * Various compiler attributes
+ */
+#define ATTR(x) __attribute__((x))
+#define PACKED  ATTR(__packed__)
+
+#endif /* !LOCALSERVER_COMMON_H */

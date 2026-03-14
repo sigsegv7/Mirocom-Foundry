b07829e14ee0c67b2a78d77a7c1f15082c3ce01f
Ian Moffett <ian@mirocom.org>
2026-03-14 06:38:34 -0400

    Srm: Foreign: Add SDK groundwork
    Signed-off-by: Ian Moffett <ian@mirocom.org>

---
+++ System/Srm/Foreign/Sdk/Include/Sdk/Types.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2026, Mirocom Laboratories
+ * Provided under the BSD-3 clause
+ */
+
+/*
+ * Description:
+ *      This header defines various system types used around the
+ *      system.
+ *  Author:
+ *      Ian M. Moffett <ian@mirocom.org.
+ */
+
+#ifndef _SDK_TYPES_H_
+#define _SDK_TYPES_H_ 1
+
+/* Unsigned types */
+typedef unsigned char        UINT8;
+typedef unsigned short       UINT16;
+typedef unsigned int         UINT32;
+typedef unsigned long long   UINT64;
+typedef UINT64               USIZE;
+
+/* Signed types */
+typedef char        INT8;
+typedef short       INT16;
+typedef int         INT32;
+typedef long long   INT64;
+typedef INT64       SSIZE;
+
+#endif  /* !_SDK_TYPES_H_ */
